a77d356e4f534b83bfaa079203031050b72dcf94
VENKATESH <127887616+dsvijayvenkat@users.noreply.github.com>
2023-04-16 02:40:37 +0530

    Add files via upload

---
+++ 1.Reading_an_Image.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Reading_an_Image_in_OpenCV
+
+# ### Reading an image by Specific Path
+
+# In[1]:
+
+
+# Importing Required Libraries
+
+import cv2
+
+# define a class called ImageReader
+class ImageReader:
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def read_image(self):
+        try:
+            img = cv2.imread(self.filename)
+            if img is None:
+                raise Exception("Error: Unable to read image file")
+        except Exception as e:
+            print(str(e))
+        else:
+            return img
+
+
+# In[2]:
+
+
+# create an instance of the ImageReader
+reader = ImageReader(r"C:\PYTHON\AI_ML\OpenCV\Computer_vision.jpeg")
+# call the read_image() method
+img = reader.read_image()
+if img is not None:
+    # to show an image
+    cv2.imshow("Image", img)
+    # to dispaly upto defined time
+    cv2.waitKey(0)
+    # to close the open window
+    cv2.destroyAllWindows()
+
+
+# After executing this code seperate window will be open with an image which has given in the path.
+
+# ### Reading an Image by allow the user to enter a link 
+
+# In[3]:
+
+
+# # Importing Required Libraries
+import cv2
+import urllib.request
+import numpy as np
+
+# define a class called ImageReader
+class ImageReader:
+    def __init__(self, url):
+        self.url = url
+        
+    def read_image(self):
+        try:
+            resp = urllib.request.urlopen(self.url)
+            img = np.asarray(bytearray(resp.read()), dtype="uint8")
+            img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+            if img is None:
+                raise Exception("Error: Unable to read image file")
+        except Exception as e:
+            print(str(e))
+        else:
+            return img
+
+
+# In[4]:
+
+
+url = input("Enter the URL of the image file: ")
+reader = ImageReader(url)
+img = reader.read_image()
+if img is not None:
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+# After Executing above code It will ask you to enter an url link to diaplay an image.
+
+# In[ ]:
+
+
+
+
